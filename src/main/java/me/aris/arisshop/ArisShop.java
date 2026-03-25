3f4ffc856bcb0dca523ec293127e900c477ff46e
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-25 12:45:35 +0700

    Update ArisShop.java

--- src/main/java/me/aris/arisshop/ArisShop.java
+++ src/main/java/me/aris/arisshop/ArisShop.java
@@ -1,29 +1,36 @@
 package me.aris.arisshop;
 
 import me.aris.arisshop.commands.ShopCommand;
-import me.aris.arisshop.utils.*;
 import me.aris.arisshop.shards.ArisShardsManager;
+import me.aris.arisshop.utils.*;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
 
 public class ArisShop extends JavaPlugin {
     private static ArisShop instance;
     private ArisShardsManager shardsManager;
-    private EconomyManager economyManager;
-    private LangManager langManager;
 
     @Override
     public void onEnable() {
         instance = this;
-        saveDefaultConfig();
-        this.langManager = new LangManager(this);
-        this.economyManager = new EconomyManager(this);
-        this.shardsManager = new ArisShardsManager(this);
         
+        String[] resources = {
+            "config.yml", "lang.yml", "shards.yml",
+            "gui/maingui.yml", "gui/buy.yml",
+            "shops/food.yml", "shops/gear.yml", "shops/end.yml", "shops/nether.yml"
+        };
+        
+        for (String res : resources) {
+            File file = new File(getDataFolder(), res);
+            if (!file.exists()) {
+                saveResource(res, false);
+            }
+        }
+
+        this.shardsManager = new ArisShardsManager(this);
         getCommand("arisshop").setExecutor(new ShopCommand());
     }
 
     public static ArisShop getInstance() { return instance; }
     public ArisShardsManager getShardsManager() { return shardsManager; }
-    public EconomyManager getEconomyManager() { return economyManager; }
-    public LangManager getLangManager() { return langManager; }
 }
