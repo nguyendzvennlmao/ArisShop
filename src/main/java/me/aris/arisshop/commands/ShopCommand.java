8dea5258a7265d7dbd1f2f232a63bec88f58a03a
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-25 11:35:40 +0700

    Create ShopCommand.java

---
+++ src/main/java/me/aris/arisshop/commands/ShopCommand.java
@@ -0,0 +1,20 @@
+package me.aris.arisshop.commands;
+
+import me.aris.arisshop.ArisShop;
+import me.aris.arisshop.models.CategoryInventory;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ShopCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) return true;
+        Player player = (Player) sender;
+        
+        CategoryInventory gui = new CategoryInventory();
+        gui.openCategoryMenu(player);
+        return true;
+    }
+    }
