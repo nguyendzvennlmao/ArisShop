bd4d69cce8f4a4766d546fc9c88143f57d3b0188
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-25 11:40:32 +0700

    Update ArisShop.java

--- src/main/java/me/aris/arisshop/ArisShop.java
+++ src/main/java/me/aris/arisshop/ArisShop.java
@@ -7,16 +7,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ArisShop extends JavaPlugin {
     private static ArisShop instance;
+    private ArisShardsManager shardsManager;
+    private EconomyManager economyManager;
+    private LangManager langManager;
 
     @Override
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
+        this.langManager = new LangManager(this);
+        this.economyManager = new EconomyManager(this);
+        this.shardsManager = new ArisShardsManager(this);
         
         getCommand("arisshop").setExecutor(new ShopCommand());
-        
-        getLogger().info("ArisShop da kich hoat lenh /shop!");
     }
 
     public static ArisShop getInstance() { return instance; }
+    public ArisShardsManager getShardsManager() { return shardsManager; }
+    public EconomyManager getEconomyManager() { return economyManager; }
+    public LangManager getLangManager() { return langManager; }
 }
