a76aa678de7e07edef23820a8baf823b75dbe5cf
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-25 13:03:49 +0700

    Update ArisShop.java

--- src/main/java/me/aris/arisshop/ArisShop.java
+++ src/main/java/me/aris/arisshop/ArisShop.java
@@ -1,36 +1,24 @@
 package me.aris.arisshop;
 
 import me.aris.arisshop.commands.ShopCommand;
-import me.aris.arisshop.shards.ArisShardsManager;
-import me.aris.arisshop.utils.*;
+import me.aris.arisshop.listeners.ShopListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.io.File;
 
 public class ArisShop extends JavaPlugin {
     private static ArisShop instance;
-    private ArisShardsManager shardsManager;
 
     @Override
     public void onEnable() {
         instance = this;
+        saveDefaultConfig();
+        saveResource("gui/maingui.yml", false);
+        saveResource("gui/buy.yml", false);
+        saveResource("shops/food.yml", false);
         
-        String[] resources = {
-            "config.yml", "lang.yml", "shards.yml",
-            "gui/maingui.yml", "gui/buy.yml",
-            "shops/food.yml", "shops/gear.yml", "shops/end.yml", "shops/nether.yml"
-        };
-        
-        for (String res : resources) {
-            File file = new File(getDataFolder(), res);
-            if (!file.exists()) {
-                saveResource(res, false);
-            }
-        }
-
-        this.shardsManager = new ArisShardsManager(this);
         getCommand("arisshop").setExecutor(new ShopCommand());
+        Bukkit.getPluginManager().registerEvents(new ShopListener(), this);
     }
 
     public static ArisShop getInstance() { return instance; }
-    public ArisShardsManager getShardsManager() { return shardsManager; }
 }
