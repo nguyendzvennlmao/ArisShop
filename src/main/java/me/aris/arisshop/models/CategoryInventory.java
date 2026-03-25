bc9960d0cec83574307e7e37f1046d3df181b263
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-25 12:44:56 +0700

    Update CategoryInventory.java

--- src/main/java/me/aris/arisshop/models/CategoryInventory.java
+++ src/main/java/me/aris/arisshop/models/CategoryInventory.java
@@ -1,25 +1,48 @@
 package me.aris.arisshop.models;
 
+import me.aris.arisshop.ArisShop;
+import me.aris.arisshop.utils.HexColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryInventory {
     public void openCategoryMenu(Player player) {
-        Inventory inv = Bukkit.createInventory(null, 27, "§0Aris Network - Shop");
+        File file = new File(ArisShop.getInstance().getDataFolder(), "gui/maingui.yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
         
-        ItemStack decor = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
-        ItemMeta meta = decor.getItemMeta();
-        meta.setDisplayName(" ");
-        decor.setItemMeta(meta);
-        
-        for (int i = 0; i < 27; i++) {
-            inv.setItem(i, decor);
+        String title = HexColor.format(config.getString("title", "&0Shop"));
+        int size = config.getInt("size", 27);
+        Inventory inv = Bukkit.createInventory(null, size, title);
+
+        if (config.contains("items")) {
+            for (String key : config.getConfigurationSection("items").getKeys(false)) {
+                int slot = config.getInt("items." + key + ".slot");
+                String matName = config.getString("items." + key + ".material");
+                if (matName == null) continue;
+                
+                ItemStack item = new ItemStack(Material.valueOf(matName.toUpperCase()));
+                ItemMeta meta = item.getItemMeta();
+                
+                meta.setDisplayName(HexColor.format(config.getString("items." + key + ".display_name")));
+                List<String> lore = new ArrayList<>();
+                for (String line : config.getStringList("items." + key + ".lore")) {
+                    lore.add(HexColor.format(line));
+                }
+                meta.setLore(lore);
+                
+                item.setItemMeta(meta);
+                inv.setItem(slot, item);
+            }
         }
-        
         player.openInventory(inv);
     }
-}
+        }
