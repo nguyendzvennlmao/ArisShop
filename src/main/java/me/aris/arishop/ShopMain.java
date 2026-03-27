a2cdd0262a0f3b5007a34449a9d427d4b80cee62
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-27 09:28:17 +0700

    Update ShopMain.java

--- src/main/java/me/aris/arishop/ShopMain.java
+++ src/main/java/me/aris/arishop/ShopMain.java
@@ -13,22 +13,28 @@ import java.util.List;
 public class ShopMain {
     public static void open(Player p) {
         ArisShop m = ArisShop.getInstance();
-        Inventory inv = Bukkit.createInventory(null, m.getConfig().getInt("main-menu.rows") * 9, m.color(m.getConfig().getString("main-menu.title")));
-        ConfigurationSection sec = m.getConfig().getConfigurationSection("main-menu.categories");
-        if (sec != null) {
-            for (String key : sec.getKeys(false)) {
-                ItemStack item = new ItemStack(Material.valueOf(sec.getString(key + ".material").toUpperCase()));
-                ItemMeta meta = item.getItemMeta();
-                if (meta != null) {
-                    meta.setDisplayName(m.color(sec.getString(key + ".displayname")));
-                    List<String> lore = new ArrayList<>();
-                    for (String s : sec.getStringList(key + ".lore")) lore.add(m.color(s));
-                    meta.setLore(lore);
-                    item.setItemMeta(meta);
+        try {
+            Inventory inv = Bukkit.createInventory(null, m.getConfig().getInt("main-menu.rows") * 9, m.color(m.getConfig().getString("main-menu.title")));
+            ConfigurationSection sec = m.getConfig().getConfigurationSection("main-menu.categories");
+            if (sec != null) {
+                for (String key : sec.getKeys(false)) {
+                    Material mat = Material.matchMaterial(sec.getString(key + ".material").toUpperCase());
+                    if (mat == null) mat = Material.BARRIER;
+                    ItemStack item = new ItemStack(mat);
+                    ItemMeta meta = item.getItemMeta();
+                    if (meta != null) {
+                        meta.setDisplayName(m.color(sec.getString(key + ".displayname")));
+                        List<String> lore = new ArrayList<>();
+                        for (String s : sec.getStringList(key + ".lore")) lore.add(m.color(s));
+                        meta.setLore(lore);
+                        item.setItemMeta(meta);
+                    }
+                    inv.setItem(sec.getInt(key + ".slot"), item);
                 }
-                inv.setItem(sec.getInt(key + ".slot"), item);
             }
+            p.openInventory(inv);
+        } catch (Exception e) {
+            p.sendMessage("§c[ArisShop] Lỗi Main Menu! Kiểm tra config.yml");
         }
-        p.openInventory(inv);
     }
-            }
+                                           }
