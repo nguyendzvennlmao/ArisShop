2e786a0da9a664f3fd9400c94e410e6e2d803a53
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-27 11:06:34 +0700

    Update ShopMain.java

--- src/main/java/me/aris/arishop/ShopMain.java
+++ src/main/java/me/aris/arishop/ShopMain.java
@@ -7,36 +7,27 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShopMain {
     public static void open(Player p) {
         ArisShop m = ArisShop.getInstance();
         try {
-            int rows = m.getConfig().getInt("main-menu.rows");
-            String title = m.getConfig().getString("main-menu.title");
-            Inventory inv = Bukkit.createInventory(null, rows * 9, m.color(title));
+            int rows = m.getConfig().getInt("main-menu.rows", 3);
+            Inventory inv = Bukkit.createInventory(null, rows * 9, m.color(m.getConfig().getString("main-menu.title")));
             ConfigurationSection sec = m.getConfig().getConfigurationSection("main-menu.categories");
             if (sec != null) {
                 for (String key : sec.getKeys(false)) {
-                    Material mat = Material.matchMaterial(sec.getString(key + ".material").toUpperCase());
-                    if (mat == null) mat = Material.BARRIER;
-                    ItemStack item = new ItemStack(mat);
+                    Material mat = Material.matchMaterial(sec.getString(key + ".material", "PAPER"));
+                    ItemStack item = new ItemStack(mat == null ? Material.BARRIER : mat);
                     ItemMeta meta = item.getItemMeta();
-                    if (meta != null) {
-                        meta.setDisplayName(m.color(sec.getString(key + ".displayname")));
-                        List<String> lore = new ArrayList<>();
-                        for (String s : sec.getStringList(key + ".lore")) lore.add(m.color(s));
-                        meta.setLore(lore);
-                        item.setItemMeta(meta);
-                    }
+                    meta.setDisplayName(m.color(sec.getString(key + ".displayname")));
+                    meta.setLore(sec.getStringList(key + ".lore").stream().map(m::color).collect(Collectors.toList()));
+                    item.setItemMeta(meta);
                     inv.setItem(sec.getInt(key + ".slot"), item);
                 }
             }
             p.openInventory(inv);
-        } catch (Exception e) {
-            p.sendMessage("§c[ArisShop] Lỗi Main Menu! Hãy kiểm tra config.yml");
-        }
+        } catch (Exception e) { p.sendMessage("§c[ArisShop] Lỗi Main Menu!"); }
     }
-                                                   }
+}
