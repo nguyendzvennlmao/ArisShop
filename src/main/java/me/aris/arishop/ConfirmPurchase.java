10a4c8720f5de07f59ba760906bdca7713515765
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-28 07:43:16 +0700

    Update ConfirmPurchase.java

--- src/main/java/me/aris/arishop/ConfirmPurchase.java
+++ src/main/java/me/aris/arishop/ConfirmPurchase.java
@@ -15,35 +15,32 @@ public class ConfirmPurchase {
     public static void open(Player p, double price, ItemStack shopItem, int amount) {
         ArisShop m = ArisShop.getInstance();
         ConfigurationSection gui = m.getConfig().getConfigurationSection("gui.quantity-selector");
-        Inventory inv = Bukkit.createInventory(null, gui.getInt("rows") * 9, m.color(gui.getString("title")));
-
+        Inventory inv = Bukkit.createInventory(null, gui.getInt("rows", 3) * 9, m.color(gui.getString("title")));
         p.setMetadata("aris_price", new FixedMetadataValue(m, price));
         p.setMetadata("aris_amount", new FixedMetadataValue(m, amount));
         p.setMetadata("aris_item", new FixedMetadataValue(m, shopItem));
-
         String[] buttons = {"confirm", "cancel", "add1", "add10", "set64", "remove1", "remove10", "remove64"};
         for (String b : buttons) {
             ConfigurationSection sec = gui.getConfigurationSection(b);
-            if (sec == null) continue;
-            ItemStack item = new ItemStack(Material.valueOf(sec.getString("material")));
-            ItemMeta meta = item.getItemMeta();
-            meta.setDisplayName(m.color(sec.getString("name")));
-            item.setItemMeta(meta);
-            inv.setItem(sec.getInt("slot"), item);
+            if (sec != null) {
+                ItemStack item = new ItemStack(Material.valueOf(sec.getString("material")));
+                ItemMeta meta = item.getItemMeta();
+                if (meta != null) meta.setDisplayName(m.color(sec.getString("name")));
+                item.setItemMeta(meta);
+                inv.setItem(sec.getInt("slot"), item);
+            }
         }
-
         ItemStack preview = shopItem.clone();
         ItemMeta pMeta = preview.getItemMeta();
-        List<String> lore = new ArrayList<>();
-        for (String s : gui.getStringList("item-preview.lore")) {
-            lore.add(m.color(s.replace("%price%", m.format(price))
-                             .replace("%amount%", String.valueOf(amount))
-                             .replace("%total_price%", m.format(price * amount))));
+        if (pMeta != null) {
+            List<String> lore = new ArrayList<>();
+            for (String s : gui.getStringList("item-preview.lore")) {
+                lore.add(m.color(s.replace("%price%", m.format(price)).replace("%amount%", String.valueOf(amount)).replace("%total_price%", m.format(price * amount))));
+            }
+            pMeta.setLore(lore);
+            preview.setItemMeta(pMeta);
         }
-        pMeta.setLore(lore);
-        preview.setItemMeta(pMeta);
         inv.setItem(gui.getInt("item-preview.slot"), preview);
-
         p.openInventory(inv);
     }
-            }
+}
